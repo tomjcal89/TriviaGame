0286b5b65edee22cdc0ef3a5ac9bf5e71d906986
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-14 23:14:10 -0700

    made timer but it doesnt show up on the page right away... need to figure out why it shows up after a second...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,10 @@ var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 
+var time = 10;
+var intervalId;
+
+
 // make an array of answers for the game?
 var answers = ["300", "Turkey", "New York"];
 
@@ -12,17 +16,36 @@ $(".hidden, .secondHidden").hide();
 
 //showing all elements besides final score and reset button
 $("#startId").click(function () {
+    run()
     $(".hidden").show();
     $("#startId").hide();
+    
 });
 
-$("#doneId").click(function (){
+$("#doneId").click(function () {
     $(".secondHidden").show();
     $(".hidden").hide();
-
 });
 
 //timer that starts counting down on a new page once the "start" button was clicked
+
+function run() {
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    
+    $(".timerClass").text("Time Remaining: " + time + " Seconds");
+    time--;
+
+    if (time === -1) {
+        $(".secondHidden").show();
+        $(".hidden").hide();
+    };
+};
+
+
+
 
 //show the listed questions after the stat button is clicked
 
