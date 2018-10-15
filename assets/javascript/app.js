a460516c7bb8a6e21f80cdca3a59af64f320284b
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-14 21:25:02 -0700

    have buttons working to display different content. ex: when start button is pressed, it diapears and the quetions appear in its place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,34 @@
+
 // make a selection of var's for the game
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
 
 // make an array of answers for the game?
+var answers = ["300", "Turkey", "New York"];
 
-//make the start page and only display the start button before it is pressed. 
+//hide all questions and timer until the "start" button is clicked
+$(".hidden, .secondHidden").hide();
+
+//showing all elements besides final score and reset button
+$("#startId").click(function () {
+    $(".hidden").show();
+    $("#startId").hide();
+});
+
+$("#doneId").click(function (){
+    $(".secondHidden").show();
+    $(".hidden").hide();
+
+});
+
+//timer that starts counting down on a new page once the "start" button was clicked
+
+//show the listed questions after the stat button is clicked
+
+//tally the correct, incorrect and unanswered questions
+
+
+//when the "Finished!" button is clicked, new page will show tallying "correct", "incorrect" and unanswered questions
+
+
