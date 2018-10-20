92dfb9e262d26e48efee6fd2e996330cdc052336
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-19 22:48:55 -0700

    corrected game and put questions and answers in js file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,42 +4,60 @@
 var wins = 0;
 var loss = 0;
 var unanswered = 0;
-var totalQues = 10;
-var time = 60;
+var time = 50;
 var intervalId;
-var answers = ["300", "Turkey", "New York", "Japan", "Las Vegas", "Golden", "None", "Reno", "16", "15"];
-
-// make function to calulate the scores when they press the finished button.
-function submitAnswers() {
-
-    //listing the questions
-    var q1 = document.forms["trivia"]["q1"].value;
-    var q2 = document.forms["trivia"]["q2"].value;
-    var q3 = document.forms["trivia"]["q3"].value;
-    var q4 = document.forms["trivia"]["q4"].value;
-    var q5 = document.forms["trivia"]["q5"].value;
-    var q6 = document.forms["trivia"]["q6"].value;
-    var q7 = document.forms["trivia"]["q7"].value;
-    var q8 = document.forms["trivia"]["q8"].value;
-    var q9 = document.forms["trivia"]["q9"].value;
-    var q10 = document.forms["trivia"]["q10"].value;
-
-    //for loop to check for unanswered questions
-
-    for (i = 1; i <= totalQues; i++) {
-        if (eval("q" + i) == null || eval("q" + i) == "") {
-            unanswered++;
-            document.getElementById("unansweredId").innerHTML =
-                ("Unanswered: " + unanswered)
-        } else if (eval("q" + i) !== answers[i - 1] || null || "") {
-            loss++;
-            document.getElementById("incorrectId").innerHTML = ("Incorrect: " + loss);
-        } else if (eval("q" + i) == answers[i - 1]) {
-            wins++;
-            document.getElementById("correctId").innerHTML = ("Correct: " + wins);
-        }
-    }
-}
+var questions = [
+    {
+        question: "1. In bowling, what score is refered to as a 'Perfect Score'?",
+        options: ["A. 250", "B. 300", "C. 400", "D. 350"],
+        answer: "B. 300"
+    },
+    {
+        question: "2. When you get 3 strikes in a row, what is it called?",
+        options: ["A. Triple", "B. Turkey", "C. Horse", "D. Eagle"],
+        answer: "B. Turkey"
+    },
+    {
+        question: "3. Where were the first indoor bowling lanes built?",
+        options: ["A. Baltimore", "B. Chicago", "C. Detroit", "D. New York"],
+        answer: "D. New York"
+    },
+    {
+        question: "4. Where is the largest bowling center in the world?",
+        options: ["A. Japan", "B. China", "C. London", "D. Denmark"],
+        answer: "A. Japan"
+    },
+    {
+        question: "5. Where is the largest US bowling center?",
+        options: ["A. Las Vegas", "B. Mall of America", "C. San Diego", "D. Miami"],
+        answer: "A. Las Vegas"
+    },
+    {
+        question: "6. When you get 9 strikes in a row, what is it called?",
+        options: ["A. 9th Strike", "B. Golden Turkey", "C. Fire Lane", "D. Golden Horse"],
+        answer: "B. Golden Turkey"
+    },
+    {
+        question: "7. What is the minimum weight required for a bowling ball?",
+        options: ["A. 2 Pounds", "B. 4 Pounds", "C. 3 Pounds", "D. No minimum weight requirement"],
+        answer: "D. No minimum weight requirement"
+    },
+    {
+        question: "8. _________ is the home to a Bowling Stadium!",
+        options: ["A. Reno, NV", "B. New York, NY", "C. Chicago, IL", "D. Portland, OR"],
+        answer: "A. Reno, NV"
+    },
+    {
+        question: "9. A bowling ball is allowed to weigh up to how many pounds?",
+        options: ["A. 20 lbs", "B. 18 lbs", "C. 16 lbs", "D.22 lbs"],
+        answer: "C. 16 lbs"
+    },
+    {
+        question: "10. A bowling pin is required to be how many inches in height?",
+        options: ["A. 20 inches", "B. 25 inches", "C. 12 inches", "D. 15 inches"],
+        answer: "D. 15 inches"
+    },
+];
 
 //hide all questions and timer until the "start" button is clicked
 $(".display-4, .hidden, .display-2, .secondHidden").hide();
@@ -47,7 +65,7 @@ $(".display-4, .hidden, .display-2, .secondHidden").hide();
 // reseting the game  after player completes one round
 $("#restartId").click(function () {
     clearInterval();
-    time = 60;
+    time = 50;
     run();
     unanswered = 0;
     wins = 0;
@@ -64,8 +82,36 @@ $("#restartId").click(function () {
 $("#startId").click(function () {
     run()
     $(".hidden, .display-4").show();
+    $(".display-4").text("Time Remaining: " + time + " Seconds");
     $(".display-3, .display-2, #startId, #test ").hide();
+
+    //Looping through questions to make sure they are all listed on the page         
+    for (var j = 0; j < questions.length; j++) {
+        $(".questionsClass").append("<br>", "<br>", "<h2>" + questions[j].question + "</h2>");
+        for (var i = 0; i < questions[j].options.length; i++) {
+            $(".questionsClass").append("<input type='radio' name='" + j + "' id='question" + j + "' value='" + questions[j].options[i] + "'>", " ", "<label>" + questions[j].options[i] + "</label>", "<br>");
+        }
+    }
 });
+
+function submitAnswers() {
+
+    //for loop to check for unanswered questions
+    for (var x = 0; x < questions.length; x++) {
+        if (!$("input[id='question" + x + "']").is(":checked")) {
+            unanswered++;
+            $("#unansweredId").text("Unanswered: " + unanswered);
+        }
+        else if ($("input[id='question" + x + "']:checked").val() === questions[x].answer) {
+            wins++;
+            $("#correctId").text("Correct: " + wins);
+        }
+        else {
+            loss++;
+            $("#incorrectId").text("Incorrect: " + loss);
+        }
+    }
+};
 
 //hide questions, show timer at stopped time, correct, incorrect and unanswered scores
 $("#doneId").click(function () {
@@ -73,9 +119,9 @@ $("#doneId").click(function () {
     $(".secondHidden").show();
     $(".hidden").hide();
     $(".display-4, .display-2").show();
-    document.getElementById("correctId").innerHTML = ("Correct: " + wins);
-    document.getElementById("incorrectId").innerHTML = ("Incorrect: " + wins)
-    document.getElementById("unansweredId").innerHTML = ("Unanswered: " + unanswered)
+    $("#correctId").text("Correct: " + wins);
+    $("#incorrectId").text("Incorrect: " + wins)
+    $("#unansweredId").text("Unanswered: " + unanswered)
     return submitAnswers();
 });
 
@@ -99,9 +145,9 @@ function decrement() {
         stopTimer();
         $(".secondHidden").show();
         $(".hidden").hide();
-        document.getElementById("correctId").innerHTML = ("Correct: " + wins);
-        document.getElementById("incorrectId").innerHTML = ("Incorrect: " + loss)
-        document.getElementById("unansweredId").innerHTML = ("Unanswered: " + unanswered)
+        $("#correctId").text("Correct: " + wins);
+        $("#incorrectId").text("Incorrect: " + loss);
+        $("#unansweredId").text("Unanswered: " + unanswered);
         return submitAnswers();
     }
 }
